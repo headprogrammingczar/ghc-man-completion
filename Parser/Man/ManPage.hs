217ed931957c1aa6d18a3e30a2b80ea7f3f51485
databox <headprogrammingczar@gmail.com>
2012-06-09 10:43:49 -0400

    almost a complete parser

--- Parser/Man/ManPage.hs
+++ Parser/Man/ManPage.hs
@@ -2,11 +2,35 @@
 module Parser.Man.ManPage where
 
 import Text.Parsec
+import Data.Char
 
 -- | A list of GHC parameters and their documentation
 data Result = Result [(String, String)]
 
+type Parser = Parsec String ()
+
+-- skip past the intro to OPTIONS
+-- then skip OPTIONS, because it is just a summary
+-- accumulate these sections until we get to FILES
 parse :: Parser Result
 parse = do
+  manyTill anyChar (try (header "options"))
+  manyTill anyChar (try anyHeader)
+  -- PARSE!
+  let emptyResults = Result []
+  results <- many parseHeaders
+
+  manyTill anyChar (try (header "files"))
+  return (joinResults results)
+
+joinResults :: [Result] -> Result
+joinResults = foldr (\(Result a) (Result b) -> Result (a ++ b)) (Result [])
+
+parseHeaders :: Parser Result
+parseHeaders = do
   return (Result [])
+
+anyHeader = newline >> many1 (upper <|> space) >> newline
+
+header str = newline >> string (map toUpper str) >> newline
 
